698b04931620fad8fa38e3e2adee03fa93440a66
Palomapr92 <palo.pr.92@gmail.com>
2021-03-16 10:48:17 +0100

    pagina casi completada
    he estado haciendo todos los cambios y modificaciones

--- trabajoT2/clicks.js
+++ trabajoT2/clicks.js
@@ -1,18 +1,17 @@
-function onClick() {
-  //Comprobamos que el navegador sea compatible con el uso de sessionStorage
-  if(typeof(Storage) !== "undefined") {
-    //En caso Positico
-    if (sessionStorage.clickcount) {
-      //En caso de que hagamos click, sumamos uno al contador
-      sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
-    } else {
-      //En caso de que sea la primera vez que entramos en la pagina
-      sessionStorage.clickcount = 1;
-    }
-    //Mostramos
-    document.getElementById("contador").innerHTML = "Has visitado " + sessionStorage.clickcount + " pagina(s).";
+
+if(typeof(Storage) !== "undefined") {
+  if (sessionStorage.clickcount) {
+    sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
   } else {
-    //En caso negativo
-    alert("Sorry, your browser does not support web storage...");
+    sessionStorage.setItem("clickcount", 1);
   }
+  if(sessionStorage.clickcount>1){
+    $("#contador").text("Has visitado " + sessionStorage.clickcount + " páginas");
+  }else{
+    $("#contador").text("Has visitado " + sessionStorage.clickcount + " página");
+  }
+} else {
+  document.getElementById("contador").innerHTML = "Sorry, your browser does not support web storage...";
 }
+
+$("#prueba").text(document.title);
